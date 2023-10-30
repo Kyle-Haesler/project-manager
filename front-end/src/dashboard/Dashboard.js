3f287145ad0084464fdf870b22120308af9975da
Kyle Haesler <kyle.haesler@gmail.com>
2023-10-30 13:46:30 -0400

    Minor formatting done for page

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -8,30 +8,69 @@ import ErrorAlert from "../layout/ErrorAlert";
  *  the date for which the user wants to view reservations.
  * @returns {JSX.Element}
  */
-function Dashboard({ date }) {
-  const [reservations, setReservations] = useState([]);
-  const [reservationsError, setReservationsError] = useState(null);
-
-  useEffect(loadDashboard, [date]);
+function Dashboard() {
+  const [projects, setProjects] = useState([]);
+  const [projectsError, setProjectsError] = useState(null);
+/* NEED TO GET FUNCTIONING
+  useEffect(loadDashboard, []);
 
   function loadDashboard() {
     const abortController = new AbortController();
-    setReservationsError(null);
+    setProjectsError(null);
     listReservations({ date }, abortController.signal)
       .then(setReservations)
       .catch(setReservationsError);
     return () => abortController.abort();
   }
+  */
 
   return (
+    <div>
+    <header className="jumbotron jumbotron-fluid bg-primary text-white text-center">
+      <h1 className="display-4">Project Manager</h1>
+    </header>
     <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+      <button type="button" className="btn btn-primary">Create Project</button>
+        <div className="row justify-content-around">
+          <span className="border">
+          <div className="col-md-1 mb-4">
+            <h2>Discovery</h2>
+          </div>
+          </span>
+          <span className="border">
+          <div className="col-md-1 mb-4">
+            <h2>Waiting</h2>
+          </div>
+          </span>
+          <span className="border">
+          <div className="col-md-1 mb-4">
+            <h2>Sent</h2>
+          </div>
+          </span>
+          <span className="border">
+          <div className="col-md-1 mb-4">
+            <h2>In-Progress</h2>
+          </div>
+          </span>
+          <span className="border">
+          <div className="col-md-1 mb-4">
+            <h2>Sent</h2>
+          </div>
+          </span>
+          <span className="border">
+          <div className="col-md-1 mb-4">
+            <h2>Complete</h2>
+          </div>
+          </span>
+          <span className="border">
+          <div className="col-md-1 mb-4">
+            <h2>Archive</h2>
+          </div>
+          </span>
       </div>
-      <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <ErrorAlert error={projectsError} />
     </main>
+    </div>
   );
 }
 
