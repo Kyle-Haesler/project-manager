a06c762cb58794e798a7075e366d747ab76d5e8d
Kyle Haesler <kyle.haesler@gmail.com>
2023-11-14 14:19:42 -0500

    LoginSignup looking good, not a function form however, will need to get it working

--- front-end/src/LoginSignUp/LoginSignUp.js
+++ front-end/src/LoginSignUp/LoginSignUp.js
@@ -5,18 +5,24 @@ import email_icon from "../assets/email.png";
 import password_icon from "../assets/password.png";
 
 function LoginSignUp() {
+  const [action, setAction] = useState("Sign Up");
+
   return (
     <body>
       <div className="box">
         <div className="header">
-          <div className="text">Sign Up</div>
+          <div className="text">{action}</div>
           <div className="underline"></div>
         </div>
         <div className="inputs">
-          <div className="input">
-            <img src={user_icon} />
-            <input type="text" placeholder="Name" />
-          </div>
+          {action === "Log In" ? (
+            <div></div>
+          ) : (
+            <div className="input">
+              <img src={user_icon} />
+              <input type="text" placeholder="Name" />
+            </div>
+          )}
           <div className="input">
             <img src={email_icon} />
             <input type="email" placeholder="Email Id" />
@@ -26,12 +32,26 @@ function LoginSignUp() {
             <input type="password" placeholder="Password" />
           </div>
         </div>
-        <div className="forgot-password">
-          Forgot password? <span>Click Here!</span>
-        </div>
+        {action === "Log In" ? (
+          <div></div>
+        ) : (
+          <div className="forgot-password">
+            Forgot password? <span>Click Here!</span>
+          </div>
+        )}
         <div className="submit-container">
-          <div className="submit">Sign Up</div>
-          <div className="submit">Login</div>
+          <div
+            className={action === "Log In" ? "submit gray" : "submit"}
+            onClick={() => setAction("Sign Up")}
+          >
+            Sign Up
+          </div>
+          <div
+            className={action === "Sign Up" ? "submit gray" : "submit"}
+            onClick={() => setAction("Log In")}
+          >
+            Login
+          </div>
         </div>
       </div>
     </body>
