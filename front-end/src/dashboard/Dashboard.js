c017b406ab684bc74f546e9e4378b15868dc6341
Kyle Haesler <kyle.haesler@gmail.com>
2023-11-21 14:16:21 -0500

    Allowed categories in dashboard to be hidden by clicking on them

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -11,6 +11,15 @@ function Dashboard() {
   const [projectsError, setProjectsError] = useState(null);
   const { user, logout } = useUser();
   const history = useHistory();
+  const initialCategoryView = {
+    discovery: "view",
+    waiting: "view",
+    inProgress: "view",
+    sent: "view",
+    complete: "view",
+    archive: "view",
+  };
+  const [categoryView, setCategoryView] = useState({ ...initialCategoryView });
 
   useEffect(loadDashboard, []);
 
@@ -25,6 +34,14 @@ function Dashboard() {
 
   function createProject() {
     history.push("/new");
+  }
+  function toggleChangeCategoryView(category) {
+    console.log(categoryView);
+    if (categoryView[category] === "view") {
+      setCategoryView({ ...categoryView, [category]: "hide" });
+    } else {
+      setCategoryView({ ...categoryView, [categoryView.category]: "view" });
+    }
   }
 
   return (
@@ -53,66 +70,129 @@ function Dashboard() {
           + Create Project
         </button>
         <div className="mainContent">
-          <div className="column">
-            <h2 className="column-title">Discovery</h2>
-            <div className="column-title-underline"></div>
-            {projects.map((project) => {
-              if (
-                project.status === "Discovery" &&
-                project.user_name === user
-              ) {
-                return <ProjectCard project={project} />;
-              }
-            })}
-          </div>
-          <div className="column">
-            <h2 className="column-title">Waiting</h2>
-            <div className="column-title-underline"></div>
-            {projects.map((project) => {
-              if (project.status === "Waiting" && project.user_name === user) {
-                return <ProjectCard project={project} />;
-              }
-            })}
-          </div>
-          <div className="column">
-            <h2 className="column-title">In-Progress</h2>
-            <div className="column-title-underline"></div>
-            {projects.map((project) => {
-              if (
-                project.status === "In-Progress" &&
-                project.user_name === user
-              ) {
-                return <ProjectCard project={project} />;
-              }
-            })}
-          </div>
-          <div className="column">
-            <h2 className="column-title">Sent</h2>
-            <div className="column-title-underline"></div>
-            {projects.map((project) => {
-              if (project.status === "Sent" && project.user_name === user) {
-                return <ProjectCard project={project} />;
-              }
-            })}
-          </div>
-          <div className="column">
-            <h2 className="column-title">Complete</h2>
-            <div className="column-title-underline"></div>
-            {projects.map((project) => {
-              if (project.status === "Complete" && project.user_name === user) {
-                return <ProjectCard project={project} />;
-              }
-            })}
-          </div>
-          <div className="column">
-            <h2 className="column-title">Archive</h2>
-            <div className="column-title-underline"></div>
-            {projects.map((project) => {
-              if (project.status === "Archive" && project.user_name === user) {
-                return <ProjectCard project={project} />;
-              }
-            })}
-          </div>
+          {categoryView.discovery === "view" ? (
+            <div className="column">
+              <h2
+                className="column-title"
+                onClick={() => toggleChangeCategoryView("discovery")}
+              >
+                Discovery
+              </h2>
+              <div className="column-title-underline"></div>
+              {projects.map((project) => {
+                if (
+                  project.status === "Discovery" &&
+                  project.user_name === user
+                ) {
+                  return <ProjectCard project={project} />;
+                }
+              })}
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {categoryView.waiting === "view" ? (
+            <div className="column">
+              <h2
+                className="column-title"
+                onClick={() => toggleChangeCategoryView("waiting")}
+              >
+                Waiting
+              </h2>
+              <div className="column-title-underline"></div>
+              {projects.map((project) => {
+                if (
+                  project.status === "Waiting" &&
+                  project.user_name === user
+                ) {
+                  return <ProjectCard project={project} />;
+                }
+              })}
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {categoryView.inProgress === "view" ? (
+            <div className="column">
+              <h2
+                className="column-title"
+                onClick={() => toggleChangeCategoryView("inProgress")}
+              >
+                In-Progress
+              </h2>
+              <div className="column-title-underline"></div>
+              {projects.map((project) => {
+                if (
+                  project.status === "In-Progress" &&
+                  project.user_name === user
+                ) {
+                  return <ProjectCard project={project} />;
+                }
+              })}
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {categoryView.sent === "view" ? (
+            <div className="column">
+              <h2
+                className="column-title"
+                onClick={() => toggleChangeCategoryView("sent")}
+              >
+                Sent
+              </h2>
+              <div className="column-title-underline"></div>
+              {projects.map((project) => {
+                if (project.status === "Sent" && project.user_name === user) {
+                  return <ProjectCard project={project} />;
+                }
+              })}
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {categoryView.complete === "view" ? (
+            <div className="column">
+              <h2
+                className="column-title"
+                onClick={() => toggleChangeCategoryView("complete")}
+              >
+                Complete
+              </h2>
+              <div className="column-title-underline"></div>
+              {projects.map((project) => {
+                if (
+                  project.status === "Complete" &&
+                  project.user_name === user
+                ) {
+                  return <ProjectCard project={project} />;
+                }
+              })}
+            </div>
+          ) : (
+            <div></div>
+          )}
+          {categoryView.archive === "view" ? (
+            <div className="column">
+              <h2
+                className="column-title"
+                onClick={() => toggleChangeCategoryView("archive")}
+              >
+                Archive
+              </h2>
+              <div className="column-title-underline"></div>
+              {projects.map((project) => {
+                if (
+                  project.status === "Archive" &&
+                  project.user_name === user
+                ) {
+                  return <ProjectCard project={project} />;
+                }
+              })}
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
         <ErrorAlert error={projectsError} />
       </main>
